2a18940ea5645b7f234ad532ce1ea9094fc8c6c0
rpg <rpg@scriptumplus.ru>
2012-03-25 01:21:13 +0400

    Very cool ffi optimisation

--- bin/Release/physics.lua
+++ bin/Release/physics.lua
@@ -3,15 +3,15 @@ require 'lib.lquery.init'
 local C = cheetah
 --~ C.init('Test', 512, 512, 32, '')
 local cp = require 'lib.chipmunk'
-space = cp.cpSpaceNew()
+space = cp.SpaceNew()
 space:setGravity(cp.v(0, -100))
-ground = cp.cpSegmentShapeNew(space.staticBody, cp.v(-20, 5), cp.v(20, -5), 0)
+ground = cp.SegmentShapeNew(space.staticBody, cp.v(-20, 5), cp.v(20, -5), 0)
 ground:setFriction(1)
 space:addShape(ground)
-moment = cp.cpMomentForCircle(1, 0, 5, cp.vzero)
-ballBody = space:addBody(cp.cpBodyNew(1, moment))
+moment = cp.MomentForCircle(1, 0, 5, cp.vzero)
+ballBody = space:addBody(cp.BodyNew(1, moment))
 ballBody:setPos(cp.v(0, 15))
-ballShape = space:addShape(cp.cpCircleShapeNew(ballBody, 5, cp.vzero))
+ballShape = space:addShape(cp.CircleShapeNew(ballBody, 5, cp.vzero))
 ballShape:setFriction(0.7)
 for t = 0, 2, 1.0/60.0 do
 	pos = ballBody:getPos()
