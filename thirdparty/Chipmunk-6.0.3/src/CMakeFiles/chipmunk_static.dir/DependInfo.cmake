c52ac4f954653cf94c22c6dbb67e1da7596f08b1
rpg <rpg@scriptumplus.ru>
2012-03-26 16:31:41 +0400

    thirdparty

---
+++ thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/chipmunk.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/chipmunk.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpConstraint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpConstraint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpDampedRotarySpring.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpDampedRotarySpring.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpDampedSpring.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpDampedSpring.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpGearJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpGearJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpGrooveJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpGrooveJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpPinJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpPinJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpPivotJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpPivotJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpRatchetJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpRatchetJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpRotaryLimitJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpRotaryLimitJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpSimpleMotor.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpSimpleMotor.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/constraints/cpSlideJoint.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/constraints/cpSlideJoint.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpArbiter.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpArbiter.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpArray.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpArray.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpBB.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpBB.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpBBTree.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpBBTree.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpBody.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpBody.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpCollision.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpCollision.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpHashSet.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpHashSet.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpPolyShape.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpPolyShape.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpShape.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpShape.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpace.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpace.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpaceComponent.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpaceComponent.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpaceHash.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpaceHash.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpaceQuery.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpaceQuery.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpaceStep.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpaceStep.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSpatialIndex.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSpatialIndex.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpSweep1D.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpSweep1D.c.o"
+  "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/cpVect.c" "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/CMakeFiles/chipmunk_static.dir/cpVect.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ thirdparty/Chipmunk-6.0.3/src/cmake_install.cmake
@@ -0,0 +1,102 @@
+# Install script for directory: /home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/usr/local")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "Release")
+  ENDIF(BUILD_TYPE)
+  MESSAGE(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+ENDIF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+
+# Set the component getting installed.
+IF(NOT CMAKE_INSTALL_COMPONENT)
+  IF(COMPONENT)
+    MESSAGE(STATUS "Install component: \"${COMPONENT}\"")
+    SET(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  ELSE(COMPONENT)
+    SET(CMAKE_INSTALL_COMPONENT)
+  ENDIF(COMPONENT)
+ENDIF(NOT CMAKE_INSTALL_COMPONENT)
+
+# Install shared libraries without execute permission?
+IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
+ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FOREACH(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libchipmunk.so.6.0.3"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libchipmunk.so"
+      )
+    IF(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      FILE(RPATH_CHECK
+           FILE "${file}"
+           RPATH "")
+    ENDIF()
+  ENDFOREACH()
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/libchipmunk.so.6.0.3"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/libchipmunk.so"
+    )
+  FOREACH(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libchipmunk.so.6.0.3"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libchipmunk.so"
+      )
+    IF(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      IF(CMAKE_INSTALL_DO_STRIP)
+        EXECUTE_PROCESS(COMMAND "/usr/bin/strip" "${file}")
+      ENDIF(CMAKE_INSTALL_DO_STRIP)
+    ENDIF()
+  ENDFOREACH()
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/src/libchipmunk.a")
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/chipmunk" TYPE FILE FILES
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/chipmunk.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/chipmunk_ffi.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/chipmunk_private.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/chipmunk_types.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/chipmunk_unsafe.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpArbiter.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpBB.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpBody.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpPolyShape.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpShape.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpSpace.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpSpatialIndex.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/cpVect.h"
+    )
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/chipmunk/constraints" TYPE FILE FILES
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpConstraint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpDampedRotarySpring.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpDampedSpring.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpGearJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpGrooveJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpPinJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpPivotJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpRatchetJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpRotaryLimitJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpSimpleMotor.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/cpSlideJoint.h"
+    "/home/rpg/Игры/cheetah/thirdparty/Chipmunk-6.0.3/include/chipmunk/constraints/util.h"
+    )
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+
