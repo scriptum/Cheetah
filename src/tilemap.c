ba56c1cbc5d7315a7651c7c034e35b90d01468ff
Sam <experimental.suicide@gmail.com>
2012-05-01 10:46:58 +0400

    working on tilemap

--- src/tilemap.c
+++ src/tilemap.c
@@ -12,101 +12,57 @@ typedef struct _Tilemap {
 	Image *img;
 } Tilemap;
 */
-#define MAX_MAP_SIZE 256
-
-static inline void calculateIndexes(Tilemap *t, int tw, int th, int imgw, int imgh)
-{
-	int w = imgw / tw; // image width in tiles
-	int h = imgh / th; // image height in tiles
-	int iw, ih, i = 0; // just counters for loops
-	
-	for (iw  = 0; iw < w; iw++) {
-		for (ih = 0; ih < h; ih++) {
-			t->index[i][0] = iw * tw;
-			t->index[i][1] = ih * th;
-			t->index[i][2] = (iw + 1) * tw;
-			t->index[i][3] = (ih + 1) * th;
-			
-			i++;
-		}
-	}
-}
 
 void newTilmapInternal(Tilemap *t, const char *name) {
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	static unsigned char tmpMap[MAX_MAP_SIZE][MAX_MAP_SIZE];
-	static char tmpStr[MAX_MAP_SIZE * 4];
-	int tw, th;     // tile size (we get it from file header)
-	int iw, ih;     // just indexes for loops
-	int w, h;       // map size in tiles (we get it after index map reading)
-	int id;         // storage for incoming index from file (while reading)
+	int iw, ih, i;  // just indexes for loops
+	char tmpStr[256];
+	
 	if(!t->img->id)
 	{
 		MYERROR("Cannot get image for tile map");
 		return;
 	}
-	int imgw = t->img->w;
-	int imgh = t->img->h;
-	printf("== %s: %d\n", __FILE__, __LINE__);
+	
 	if (name == NULL)
 		MYERROR("Can't load tilemap without name");
 	
 	if (t == NULL)
 		MYERROR("Map %s not initialised", name);
-	printf("== %s: %d\n", __FILE__, __LINE__);
+	
 	FILE *f = fopen(name, "r");
 	if (f == NULL)
 		MYERROR("Can't open tilemap %s", name);
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	if (fscanf(f, "%s %d %d\n", tmpStr, &tw, &th) != 3)
-		MYERROR("Can't read tilemap's image and size from %s", name);
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	// prepare
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	// here maybe crash
-	for (iw = 0; iw < MAX_MAP_SIZE; iw++)
-		memset(tmpMap[iw], 0, MAX_MAP_SIZE * sizeof(char));
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	calculateIndexes(t, tw, th, imgw, imgh);
-	iw = ih = 0;
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	// read index map to temp map (we don't know sizes)
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	// read string
-	while (fgets(tmpStr, MAX_MAP_SIZE * 4, f) != NULL) {
-		char *pos = tmpStr;
-		ih = 0;
-		printf("== %s: %d\n", __FILE__, __LINE__);
-		// read all numbers in string
-		while (sscanf(pos, "%d\t", &id) == 1) {
-			if (id < 0 || id > 255)
-				MYERROR("Worng tilemap index from %s", name);
-			//~ printf("== %s: %d\n", __FILE__, __LINE__);
-			tmpMap[iw][ih] = id;
-			//~ printf("== %s: %d\n", __FILE__, __LINE__);
-			pos = strstr(pos, "\t");
-			if (pos == NULL) break;
-			ih++;
-		}
-		printf("== %s: %d\n", __FILE__, __LINE__);
-		iw++;
-		w = iw;
-		if (h < ih) h = ih;
-	}
-	printf("== %s: %d\n", __FILE__, __LINE__);
-	// copy temp map to our tilemap
 	
-	t->map = NULL;
-	new(t->map, unsigned char *, w);
-	for (iw = 0; iw < w; iw++) {
-		t->map[iw] = NULL;
-		new(t->map[iw], unsigned char, h);
+	if (fscanf(f, "%s %d %d %d %d\n", tmpStr, &t->w, &t->h, &t->tw, &t->th) != 5)
+		MYERROR("Can't read tilemap's image and size from %s", name);
+	
+	// calculate indexes' texture coords
+	for (iw  = 0; iw < t->w; iw++) {
+		for (ih = 0; ih < t->h; ih++) {
+			t->index[i][0] = iw * t->tw;
+			t->index[i][1] = ih * t->th;
+			t->index[i][2] = (iw + 1) * t->tw;
+			t->index[i][3] = (ih + 1) * t->th;
+			
+			i++;
+		}
 	}
 	printf("== %s: %d\n", __FILE__, __LINE__);
-	for (iw = 0; iw < w; iw ++)
-		for (ih = 0; ih < h; ih++)
-			t->map[iw][ih] = tmpMap[iw][ih];
+	// get memory
+	new(t->map, unsigned char, t->w);
+	for (i = 0; i < t->w; i++)
+		new(t->map[i], unsigned char, t->h);
 	printf("== %s: %d\n", __FILE__, __LINE__);
+	// read index map
+	for (iw = 0; iw < t->w; iw++) {
+		for (ih = 0; ih < t->h; ih++) {
+			if (fscanf(f, "%d", &i) != 1)
+				MYERROR("Can't read tilemap from %s", name);
+			t->map[iw][ih] = (unsigned char)i;
+		}
+	}
+	printf("== %s: %d\n", __FILE__, __LINE__);
+	fclose(f);
 }
 
 void tilemapDraw(Tilemap *t, float x, float y, float r, float z) {
