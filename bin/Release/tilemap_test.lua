386141634dee976c3c4653083d98417bb3bda6af
rpg <rpg@scriptumplus.ru>
2012-05-01 20:09:54 +0400

    tilemap fix

--- bin/Release/tilemap_test.lua
+++ bin/Release/tilemap_test.lua
@@ -2,14 +2,16 @@ require 'lib.cheetah'
 --~ require 'lib.mixer'
 require 'lib.lquery.init'
 local C = cheetah
-C.init('Test', 512, 512, 32, '')
-
+C.init('Test', 512, 512, 32, 'v')
+C.printFPS = true
 t = C.newTilemap("data/tilemap_test.map")
 
 e = E:new(screen):draw(function(s)
-	t:draw(s.x, s.y, s.angle, 1)
+	t:draw(math.floor(s.x), math.floor(s.y), s.angle, 1)
 end)
-:animate({x = 600}, 50)
+:animate({x = 200, y = 200}, 1)
+:size(192, 128)
+:draggable()
 
 C.mainLoop()
 
