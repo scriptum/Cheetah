be6a979b2fe6aa1cafea29d41926749b61d14ffa
rpg <rpg@scriptumplus.ru>
2012-05-01 16:14:11 +0400

    tilemap image disappear fix

--- bin/Release/tilemap_test.lua
+++ bin/Release/tilemap_test.lua
@@ -6,9 +6,10 @@ C.init('Test', 512, 512, 32, '')
 
 t = C.newTilemap("data/tilemap_test.map")
 
-e = E:new(screen):draw(function()
-	t:draw(0, 0, 0, 1)
+e = E:new(screen):draw(function(s)
+	t:draw(s.x, s.y, s.angle, 1)
 end)
+:animate({x = 600}, 50)
 
 C.mainLoop()
 
