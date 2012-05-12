2e0129b542e898dc9e5f98d0534461e1d889dbb4
rpg <rpg@scriptumplus.ru>
2012-05-12 22:39:41 +0400

    atlas images

--- bin/Release/atlas.lua
+++ bin/Release/atlas.lua
@@ -3,11 +3,11 @@ require 'lib.lquery.init'
 local C = cheetah
 C.init('Test', 512, 512, 32, 'v')
 
-local data = C.resLoader('data/atlas')
+local data = C.resLoader('data/cheetah')
 local tiger = C.resLoader('data/1111')
 
 E:new(screen):draw(function()
-	data.images.anima.player.death[0][math.floor(time * 20 % 56)]:draw(0, 256, 256, 256)
+	data[math.floor(time * 24 % 32)]:draw(0, 256)
 	tiger.tiger:draw(0,0,nil,nil,time/10)
 end)
 
