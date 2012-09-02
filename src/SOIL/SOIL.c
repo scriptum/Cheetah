5533bbfc587514446017f48e52c1c0cc696720cf
rpg <rpg@scriptumplus.ru>
2012-09-02 18:09:41 +0400

    Reduce SOIL size

--- src/SOIL/SOIL.c
+++ src/SOIL/SOIL.c
@@ -1417,82 +1417,82 @@ unsigned int
 	return tex_id;
 }
 
-int
-	SOIL_save_screenshot
-	(
-		const char *filename,
-		int image_type,
-		int x, int y,
-		int width, int height
-	)
-{
-	unsigned char *pixel_data;
-	int i, j;
-	int save_result;
+//~ int
+	//~ SOIL_save_screenshot
+	//~ (
+		//~ const char *filename,
+		//~ int image_type,
+		//~ int x, int y,
+		//~ int width, int height
+	//~ )
+//~ {
+	//~ unsigned char *pixel_data;
+	//~ int i, j;
+	//~ int save_result;
+//~ 
+	//~ /*	error checks	*/
+	//~ if( (width < 1) || (height < 1) )
+	//~ {
+		//~ result_string_pointer = "Invalid screenshot dimensions";
+		//~ return 0;
+	//~ }
+	//~ if( (x < 0) || (y < 0) )
+	//~ {
+		//~ result_string_pointer = "Invalid screenshot location";
+		//~ return 0;
+	//~ }
+	//~ if( filename == NULL )
+	//~ {
+		//~ result_string_pointer = "Invalid screenshot filename";
+		//~ return 0;
+	//~ }
+//~ 
+    //~ /*  Get the data from OpenGL	*/
+    //~ pixel_data = (unsigned char*)malloc( 3*width*height );
+    //~ glReadPixels (x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixel_data);
+//~ 
+    //~ /*	invert the image	*/
+    //~ for( j = 0; j*2 < height; ++j )
+	//~ {
+		//~ int index1 = j * width * 3;
+		//~ int index2 = (height - 1 - j) * width * 3;
+		//~ for( i = width * 3; i > 0; --i )
+		//~ {
+			//~ unsigned char temp = pixel_data[index1];
+			//~ pixel_data[index1] = pixel_data[index2];
+			//~ pixel_data[index2] = temp;
+			//~ ++index1;
+			//~ ++index2;
+		//~ }
+	//~ }
+//~ 
+    //~ /*	save the image	*/
+    //~ save_result = SOIL_save_image( filename, image_type, width, height, 3, pixel_data);
+//~ 
+    //~ /*  And free the memory	*/
+    //~ SOIL_free_image_data( pixel_data );
+	//~ return save_result;
+//~ }
 
-	/*	error checks	*/
-	if( (width < 1) || (height < 1) )
-	{
-		result_string_pointer = "Invalid screenshot dimensions";
-		return 0;
-	}
-	if( (x < 0) || (y < 0) )
-	{
-		result_string_pointer = "Invalid screenshot location";
-		return 0;
-	}
-	if( filename == NULL )
-	{
-		result_string_pointer = "Invalid screenshot filename";
-		return 0;
-	}
-
-    /*  Get the data from OpenGL	*/
-    pixel_data = (unsigned char*)malloc( 3*width*height );
-    glReadPixels (x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixel_data);
-
-    /*	invert the image	*/
-    for( j = 0; j*2 < height; ++j )
-	{
-		int index1 = j * width * 3;
-		int index2 = (height - 1 - j) * width * 3;
-		for( i = width * 3; i > 0; --i )
-		{
-			unsigned char temp = pixel_data[index1];
-			pixel_data[index1] = pixel_data[index2];
-			pixel_data[index2] = temp;
-			++index1;
-			++index2;
-		}
-	}
-
-    /*	save the image	*/
-    save_result = SOIL_save_image( filename, image_type, width, height, 3, pixel_data);
-
-    /*  And free the memory	*/
-    SOIL_free_image_data( pixel_data );
-	return save_result;
-}
-
-unsigned char*
-	SOIL_load_image
-	(
-		const char *filename,
-		int *width, int *height, int *channels,
-		int force_channels
-	)
-{
-	unsigned char *result = stbi_load( filename,
-			width, height, channels, force_channels );
-	if( result == NULL )
-	{
-		result_string_pointer = stbi_failure_reason();
-	} else
-	{
-		result_string_pointer = "Image loaded";
-	}
-	return result;
-}
+//~ unsigned char*
+	//~ SOIL_load_image
+	//~ (
+		//~ const char *filename,
+		//~ int *width, int *height, int *channels,
+		//~ int force_channels
+	//~ )
+//~ {
+	//~ unsigned char *result = stbi_load( filename,
+			//~ width, height, channels, force_channels );
+	//~ if( result == NULL )
+	//~ {
+		//~ result_string_pointer = stbi_failure_reason();
+	//~ } else
+	//~ {
+		//~ result_string_pointer = "Image loaded";
+	//~ }
+	//~ return result;
+//~ }
 
 unsigned char*
 	SOIL_load_image_from_memory
@@ -1517,52 +1517,52 @@ unsigned char*
 	return result;
 }
 
-int
-	SOIL_save_image
-	(
-		const char *filename,
-		int image_type,
-		int width, int height, int channels,
-		const unsigned char *const data
-	)
-{
-	int save_result;
-
-	/*	error check	*/
-	if( (width < 1) || (height < 1) ||
-		(channels < 1) || (channels > 4) ||
-		(data == NULL) ||
-		(filename == NULL) )
-	{
-		return 0;
-	}
-	if( image_type == SOIL_SAVE_TYPE_BMP )
-	{
-		save_result = stbi_write_bmp( filename,
-				width, height, channels, (void*)data );
-	} else
-	if( image_type == SOIL_SAVE_TYPE_TGA )
-	{
-		save_result = stbi_write_tga( filename,
-				width, height, channels, (void*)data );
-	} else
-	if( image_type == SOIL_SAVE_TYPE_DDS )
-	{
-		save_result = save_image_as_DDS( filename,
-				width, height, channels, (const unsigned char *const)data );
-	} else
-	{
-		save_result = 0;
-	}
-	if( save_result == 0 )
-	{
-		result_string_pointer = "Saving the image failed";
-	} else
-	{
-		result_string_pointer = "Image saved";
-	}
-	return save_result;
-}
+//~ int
+	//~ SOIL_save_image
+	//~ (
+		//~ const char *filename,
+		//~ int image_type,
+		//~ int width, int height, int channels,
+		//~ const unsigned char *const data
+	//~ )
+//~ {
+	//~ int save_result;
+//~ 
+	//~ /*	error check	*/
+	//~ if( (width < 1) || (height < 1) ||
+		//~ (channels < 1) || (channels > 4) ||
+		//~ (data == NULL) ||
+		//~ (filename == NULL) )
+	//~ {
+		//~ return 0;
+	//~ }
+	//~ if( image_type == SOIL_SAVE_TYPE_BMP )
+	//~ {
+		//~ save_result = stbi_write_bmp( filename,
+				//~ width, height, channels, (void*)data );
+	//~ } else
+	//~ if( image_type == SOIL_SAVE_TYPE_TGA )
+	//~ {
+		//~ save_result = stbi_write_tga( filename,
+				//~ width, height, channels, (void*)data );
+	//~ } else
+	//~ if( image_type == SOIL_SAVE_TYPE_DDS )
+	//~ {
+		//~ save_result = save_image_as_DDS( filename,
+				//~ width, height, channels, (const unsigned char *const)data );
+	//~ } else
+	//~ {
+		//~ save_result = 0;
+	//~ }
+	//~ if( save_result == 0 )
+	//~ {
+		//~ result_string_pointer = "Saving the image failed";
+	//~ } else
+	//~ {
+		//~ result_string_pointer = "Image saved";
+	//~ }
+	//~ return save_result;
+//~ }
 
 void
 	SOIL_free_image_data
