d6cb1a78e8958b6ff134e4dfbcdb0642f652c792
rpg <rpg@scriptumplus.ru>
2012-09-18 10:31:19 +0400

    Change build scripts

--- make_linux.sh
+++ make_linux.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 DIR=./bin/Release
-make
 MACHINE_NAME=`uname -m`
 case ${MACHINE_NAME} in
 i486|i586|i686)
@@ -13,4 +12,4 @@ x86_64)
   MACHINE_NAME=
   ;;
 esac
-mv $DIR/libcheetah.so $DIR/bin/linux$MACHINE_NAME/libcheetah.so
+make && mv libcheetah.so $DIR/bin/linux$MACHINE_NAME/libcheetah.so
--- make_win.sh
+++ make_win.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 DIR=./bin/Release
+FAIL=0
 whereis i586-mingw32msvc-gcc | grep / > /dev/null
 if [ $? = "1" ]
 then
-	echo "You must install mingw32 - Minimalist GNU win32 (cross) compiler"
-	exit 1
+	whereis i586-pc-mingw32-gcc | grep / > /dev/null
+	if [ $? = "1" ]
+	then
+		echo "You must install mingw32 - Minimalist GNU win32 (cross) compiler"
+		exit 1
+	else
+		MINGWGCC=i586-pc-mingw32-gcc
+	fi
+else
+	MINGWGCC=i586-mingw32msvc-gcc
 fi
-export CC=i586-mingw32msvc-gcc
-make
-mv $DIR/libcheetah.so $DIR/bin/win32/cheetah.dll
+
+export CC=$MINGWGCC
+make && mv libcheetah.so $DIR/bin/win32/cheetah.dll
