e354c41b2724b26e23acfb2172b072c313c80a97
rpg <rpg@scriptumplus.ru>
2012-10-20 00:08:26 +0400

    New vertex processor

---
+++ src/config.h
@@ -0,0 +1,4 @@
+/* Some Cheetah's tuning values */
+
+/* Limit to virtual vertex buffer, after this limit engine flushes verticles */
+#define VERTEX_BUFFER_LIMIT 4096 /* 4096 verticles (=256 Kb) */
