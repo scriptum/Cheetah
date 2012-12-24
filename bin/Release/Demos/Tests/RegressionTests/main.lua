c84acffa3e8c6e5f3ced5bc3c1e751dc3205e02c
rpg <rpg@scriptumplus.ru>
2012-12-24 22:33:21 +0400

    Test update

---
+++ bin/Release/Demos/Tests/RegressionTests/main.lua
@@ -0,0 +1,20 @@
+require 'lib.cheetah'
+require 'lib.lquery.init'
+local C = cheetah
+C.init('Regression tests', 1, 1)
+--test dds images
+local ddsimg = C.newImage("01.dds")
+--test npot png images
+local npotimg = C.newImage("npot.png")
+local fbo = C.newFramebuffer(2048,2048)
+fbo:bind()
+--testing scaling
+ddsimg:draw(0,0,2048,2048)
+--testing instant draw
+npotimg:draw()
+--testing instant draw
+npotimg:draw(0,19)
+--testing rotation
+npotimg:draw(npotimg.w * 0.5, npotimg.h, nil, nil, math.pi/180, npotimg.w * 0.5, npotimg.h * 0.5)
+--save result
+fbo:save("01.bmp")
