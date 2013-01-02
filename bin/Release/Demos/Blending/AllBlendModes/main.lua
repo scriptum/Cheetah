eb29339c970436ebf94eb392fdafb13fee6ab27b
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-02 20:30:31 +0400

    fix blend example

--- bin/Release/Demos/Blending/AllBlendModes/main.lua
+++ bin/Release/Demos/Blending/AllBlendModes/main.lua
@@ -1,11 +1,10 @@
 require 'lib.cheetah'
 require 'lib.lquery.init'
 local C = cheetah
-C.init('Test', 1024, 512+64, 32, 'v')
-C.printFPS = true
-local img = C.generate('light', 32, 32)
-local img1 = C.newImage('sprite.png')
-lQuery.addhook(C.clear)
+C.init('Test', '1024x'..(512)..' vsync')
+
+local gray = C.generate('light', 64, 64)
+local img = C.newImage('sprite.png')
 E:new(screen):image('brick.jpg')
 :size(1024, 1024)
 local blend1 = {
@@ -30,35 +29,23 @@ local blend2 = {
 	cheetah.GL_ONE_MINUS_DST_ALPHA
 }
 E:new(screen):draw(function()
-	C.move(0, 0)
 	C.color(255,255,255,255)
 	--~ C.setBlendEquation(cheetah.GL_MAX)
 	
 	for i, u in ipairs(blend1) do
-		C.push()
 		for j, v in ipairs(blend2) do
 			C.blendFunc(u, v)
-			--~ C.setBlendMode(C.blendMask)
-			
-			img1:draw()
-			C.move(1, 0)
+			img:draw((i-1)*64, (j-1)*64)
 		end
-		C.pop()
-		C.move(0, 1)
 	end
-	C.move(8, -9)
 	for i, u in ipairs(blend1) do
-		C.push()
 		for j, v in ipairs(blend2) do
 			C.blendFunc(u, v)
-			img:draw()
-			C.move(1, 0)
+			gray:draw((i+8)*64, (j-1)*64)
 		end
-		C.pop()
-		C.move(0, 1)
 	end
 	C.blendEquation(cheetah.GL_FUNC_ADD)
 	C.blendMode(0)
-end):size(64, 64):translate()
+end)
 
 C.mainLoop()
