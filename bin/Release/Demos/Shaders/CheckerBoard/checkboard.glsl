a16fdd4119ee3b76f2745a92d44bdc1f485dee2f
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-03 07:47:57 +0400

    checkerboard shader

---
+++ bin/Release/Demos/Shaders/CheckerBoard/checkboard.glsl
@@ -0,0 +1,11 @@
+#version 120
+uniform vec2 size;
+uniform vec2 scale;
+uniform vec2 offset;
+uniform vec3 color1;
+uniform vec3 color2;
+void main() {
+	vec2 tc = (gl_TexCoord[0].st * scale - offset) / size;
+	vec3 c = vec3(mix(color2, color1, (cos(tc.x-tc.y) - cos(tc.x+tc.y)) * 10e30));
+	gl_FragColor = vec4(c, 1.0);
+}
