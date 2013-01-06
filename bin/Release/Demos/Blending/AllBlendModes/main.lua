ad147b76e6b74ad50d65ad9232192090e07e7b66
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-07 00:58:29 +0400

    demo fixes

--- bin/Release/Demos/Blending/AllBlendModes/main.lua
+++ bin/Release/Demos/Blending/AllBlendModes/main.lua
@@ -1,7 +1,7 @@
 require 'lib.cheetah'
 require 'lib.lquery.init'
 local C = cheetah
-C.init('Test', '1024x512 vsync')
+C.init('Blend modes', '1024x512 vsync')
 
 local gray = C.generate('light', 64, 64)
 local img = C.newImage('sprite.png')
@@ -29,9 +29,9 @@ local blend2 = {
 	cheetah.GL_ONE_MINUS_DST_ALPHA
 }
 E:new(screen):draw(function()
-	C.color(255,255,255,255)
-	--~ C.blendEquation(cheetah.GL_MAX)
-	
+	C.color(0,0,0,0)
+	--~ C.blendEquation(cheetah.GL_FUNC_REVERSE_SUBTRACT)
+	--~ C.texEnv(C.GL_TEXTURE_ENV, C.GL_SRC0_RGB, C.GL_CONSTANT)
 	for i, u in ipairs(blend1) do
 		for j, v in ipairs(blend2) do
 			C.blendFunc(u, v)
@@ -44,7 +44,8 @@ E:new(screen):draw(function()
 			gray:draw((i+8)*64, (j-1)*64)
 		end
 	end
-	--~ C.blendEquation(cheetah.GL_FUNC_ADD)
+	C.color(255,255,255,255)
+	C.blendEquation(cheetah.GL_FUNC_ADD)
 	C.blendMode(0)
 end)
 
