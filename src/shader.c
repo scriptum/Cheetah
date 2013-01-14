f3c6f76d59106b1d9cf8568d1b3f0db659655c25
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-14 22:05:14 +0400

    copyright fix

--- src/shader.c
+++ src/shader.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
-Copyright (c) 2012 Pavel Roschin (aka RPG) <rpg89@post.ru>
+Copyright (c) 2012-2013 Pavel Roschin (aka RPG) <rpg89@post.ru>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy 
 of this software and associated documentation files (the "Software"), to 
