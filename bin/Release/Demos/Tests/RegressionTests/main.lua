3e618128166f19439727f3ae676158734e37b41e
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-14 22:04:51 +0400

    regression tests update

--- bin/Release/Demos/Tests/RegressionTests/main.lua
+++ bin/Release/Demos/Tests/RegressionTests/main.lua
@@ -1,7 +1,7 @@
 require 'lib.cheetah'
 require 'lib.lquery.init'
 local C = cheetah
-C.init('Regression tests', 1, 1)
+C.init('Regression tests', '1x1 noframe')
 --test dds images
 local ddsimg = C.newImage("01.dds")
 --test npot png images
@@ -16,5 +16,7 @@ npotimg:draw()
 npotimg:draw(0,19)
 --testing rotation
 npotimg:draw(npotimg.w * 0.5, npotimg.h, nil, nil, math.pi/180, npotimg.w * 0.5, npotimg.h * 0.5)
+npotimg:draw(0, npotimg.h*2, npotimg.w, npotimg.h, -math.pi/180)
+npotimg:draw(npotimg.w, npotimg.h*4, npotimg.w, npotimg.h, math.pi/180, npotimg.w, npotimg.h)
 --save result
-fbo:save("01.bmp")
+fbo:save("result.bmp")
