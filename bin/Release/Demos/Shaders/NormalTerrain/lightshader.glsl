aa141fa5fe2a2bd3408eb789afab432e0205b02e
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-15 21:49:10 +0400

    shader api fixes

--- bin/Release/Demos/Shaders/NormalTerrain/lightshader.glsl
+++ bin/Release/Demos/Shaders/NormalTerrain/lightshader.glsl
@@ -6,12 +6,13 @@ uniform vec2 screen;
 uniform float height; 
 //uniform float shininess;
 //uniform float specular;
+varying vec2 TexCoord;
 void main() {
-	vec3 texc = texture2D(light, gl_TexCoord[0].st).rgb; 
+	vec3 texc = texture2D(light, TexCoord).rgb; 
 	// Extract the normal from the normal map
 	vec4 normal_c = texture2D(normal_tex, gl_FragCoord.xy/screen);
 	vec3 normal = normal_c.rgb * 2.0 - 1.0;  
-	float diffuse = dot(normal, normalize(vec3(0.5 - gl_TexCoord[0].st, height)));
+	float diffuse = dot(normal, normalize(vec3(0.5 - TexCoord, height)));
 
 	// Set the output color of our current pixel
 	gl_FragColor = vec4(diffuse*texc*lightColor, 1.0);
