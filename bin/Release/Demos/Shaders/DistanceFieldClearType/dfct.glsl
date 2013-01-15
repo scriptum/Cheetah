31611d5a0021f14e61e3a179906f5f7502c64985
RPG <rpg@scriptumplus.ru>
2013-01-15 09:33:36 +0400

    New shader api fix

--- bin/Release/Demos/Shaders/DistanceFieldClearType/dfct.glsl
+++ bin/Release/Demos/Shaders/DistanceFieldClearType/dfct.glsl
@@ -3,11 +3,12 @@ uniform sampler2D texture;
 uniform float sharpness;
 uniform float gamma;
 uniform float pixel;
+varying vec2 TexCoord;
 
 void main() {
 	vec4 color = texture2D(texture, gl_TexCoord[0].xy);
-	float alpha_left  = texture2D(texture, gl_TexCoord[0].xy - vec2(pixel * 0.5, 0.)).a;
-	float alpha_right = texture2D(texture, gl_TexCoord[0].xy + vec2(pixel * 0.5, 0.)).a;
+	float alpha_left  = texture2D(texture, TexCoord - vec2(pixel * 0.5, 0.)).a;
+	float alpha_right = texture2D(texture, TexCoord + vec2(pixel * 0.5, 0.)).a;
 	gl_FragColor = vec4(vec3(
 	smoothstep(gamma + sharpness, gamma - sharpness, alpha_left),
 	smoothstep(gamma + sharpness, gamma - sharpness, color.a),
