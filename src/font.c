b9e33a30bb96c562765f373450d7a33af43d91e7
Pavel Roschin <roshin@scriptumplus.ru>
2013-01-30 22:53:14 +0400

    add hashing font

--- src/font.c
+++ src/font.c
@@ -2,21 +2,21 @@
 
 Copyright (c) 2012-2013 Pavel Roschin (aka RPG) <rpg89@post.ru>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to 
-deal in the Software without restriction, including without limitation the 
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:  The above 
-copyright notice and this permission notice shall be included in all copies 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:  The above
+copyright notice and this permission notice shall be included in all copies
 or substantial portions of the Software.
- 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 
 *******************************************************************************/
@@ -33,6 +33,19 @@ IN THE SOFTWARE.
 #include "macros.h"
 #include "render.h"
 #include "vertex.h"
+#include "chash.h"
+
+inline unsigned fontHashFunc(unsigned key)
+{
+	return key ^ (key << 7) ^ (key >> 5);
+}
+
+inline unsigned fontCmpFunc(unsigned a, unsigned b)
+{
+	return a == b;
+}
+
+HASH_TEMPLATE(FontHash, unsigned, FontChar*, fontHashFunc, fontCmpFunc)
 
 void imageBind(Image * image);
 extern SDL_Surface *screen;
@@ -41,7 +54,7 @@ extern SDL_Surface *screen;
  * @descr Calculate width of string.
  * @group font
  * @var Font object
- * @var string 
+ * @var string
  * @return length of string in pixels
  * */
 float fontWidth(Font *f, register const char *str) {
@@ -87,47 +100,42 @@ float Font_Width(Font *f, register const char *str)
 
 
 #define DRAW_CHAR do {                                                         \
-	if(vertexCounter >= VERTEX_BUFFER_LIMIT * 8) { FLUSH_BUFFER(); }             \
-	texCoord[vertexCounter] = texCoord[vertexCounter+2] = ch->t[0];              \
-	texCoord[vertexCounter+1] = texCoord[vertexCounter+7] = ch->t[1];            \
-	texCoord[vertexCounter+3] = texCoord[vertexCounter+5] = ch->t[3];            \
-	texCoord[vertexCounter+4] = texCoord[vertexCounter+6] = ch->t[2];            \
-	w = ceilf(x);                                                                 \
-	vertexCoord[vertexCounter] = vertexCoord[vertexCounter+2] = ch->v[0] + w;    \
-	vertexCoord[vertexCounter+1] = vertexCoord[vertexCounter+7] = ch->v[1] + h;  \
-	vertexCoord[vertexCounter+3] = vertexCoord[vertexCounter+5] = ch->v[3] + h;  \
-	vertexCoord[vertexCounter+4] = vertexCoord[vertexCounter+6] = ch->v[2] + w;  \
-	vertexCounter += 8;                                                          \
-	x += ch->w;                                                                  \
+    if(vertexCounter >= VERTEX_BUFFER_LIMIT * 8) { FLUSH_BUFFER(); }           \
+    texCoord[vertexCounter] = texCoord[vertexCounter+2] = ch->t[0];            \
+    texCoord[vertexCounter+1] = texCoord[vertexCounter+7] = ch->t[1];          \
+    texCoord[vertexCounter+3] = texCoord[vertexCounter+5] = ch->t[3];          \
+    texCoord[vertexCounter+4] = texCoord[vertexCounter+6] = ch->t[2];          \
+    w = ceil(x);                                                               \
+    vertexCoord[vertexCounter] = vertexCoord[vertexCounter+2] = ch->v[0] + w;  \
+    vertexCoord[vertexCounter+1] = vertexCoord[vertexCounter+7] = ch->v[1] + h;\
+    vertexCoord[vertexCounter+3] = vertexCoord[vertexCounter+5] = ch->v[3] + h;\
+    vertexCoord[vertexCounter+4] = vertexCoord[vertexCounter+6] = ch->v[2] + w;\
+    vertexCounter += 8;                                                        \
+    x += ch->w;                                                                \
 }                                                                              \
 while(0)
 
 #define UNICODE_TO_INT(a, i, increment)                                        \
-high = low = 0;                                                                \
-if((a[i] & 0x80) == 0) {                                                       \
-	low = a[i];                                                                  \
-	increment=1;                                                                 \
+if((a[i] & 0b10000000) == 0) {                                                 \
+    c = a[i];  increment=1;                                                    \
 }                                                                              \
-else if((a[i] & 0xe0) == 0xc0) {                                               \
-	low = a[i+1] & 0x3f;                                                         \
-	low |= (a[i] & 0x03) << 6;                                                   \
-	high = (a[i] & 0x1f) >> 2;                                                   \
-	increment=2;                                                                 \
+else if((a[i] & 0b11100000) == 0b11000000) {                                   \
+    c = (a[i] & 0b00011111) << 6;                                              \
+    c |= a[i+1] & 0b00111111;                                                  \
+    increment=2;                                                               \
 }                                                                              \
-else if((a[i] & 0xf0) == 0xe0) {                                               \
-	low = a[i+2] & 0x3f;                                                         \
-	low |= (a[i+1] & 0x03) << 6;                                                 \
-	high = (a[i+1] & 0x3f) >> 2;                                                 \
-	high |= (a[i] & 0x0f) << 4;                                                  \
-	increment=3;                                                                 \
+else if((a[i] & 0b11110000) == 0b11100000) {                                   \
+    c = (a[i] & 0b00001111) << 12;                                             \
+    c |= (a[i+1] & 0b00111111) << 6;                                           \
+    c |= a[i+2] & 0b00111111;                                                  \
+    increment=3;                                                               \
 }                                                                              \
-else if((a[i] & 0xf8) == 0xf0) {                                               \
-	low = a[i+3] & 0x3f;                                                         \
-	low |= (a[i+2] & 0x03) << 6;                                                 \
-	high = (a[i+2] & 0x3f) >> 2;                                                 \
-	high |= (a[i+1] & 0x3f) << 4;                                                \
-	high |= (a[i] & 0x07) << 10;                                                 \
-	increment=4;                                                                 \
+else if((a[i] & 0b11111000) == 0b11110000) {                                   \
+    c = (a[i] & 0b00000111) << 18;                                             \
+    c |= (a[i+1] & 0b00111111) << 12;                                          \
+    c |= (a[i+2] & 0b00111111) << 6;                                           \
+    c |= a[i+3] & 0b00111111;                                                  \
+    increment=4;                                                               \
 }
 
 void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, float maxw, int align) {
@@ -136,11 +144,9 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 	int       last_space   = 0;
 	int       buf          = 0;
 	int       spaces       = -1;
-	int       high         = 0;
-	int       low          = 0;
 	int       increment    = 0;
 	int       incrementBuf = 0;
-	int       c;
+	unsigned  c;
 	float     w            = 0.0;
 	float     h            = 0.0;
 	float     lastw        = 0.0;
@@ -150,6 +156,9 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 	float     oldy         = y;
 	bool      end          = FALSE;
 	bool      justify      = (align == alignJustify);
+	FontHash *hash         = (FontHash*)currentFont->hash;
+	if(NULL == hash)
+		return;
 	if(maxw > 0.0)
 	{
 		maxw = maxw / currentFont->_scale * screenScale.scaleX;
@@ -167,25 +176,28 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 		glScalef(currentFont->_scale, currentFont->_scale, 1);
 		glTranslatef(x, y, 0);
 	}
-	
+
 	x = h = y = 0.0;
 
 	imageBind(currentFont->image);
-	#define CHAR_NOT_EXISTS (high > currentFont->allocated || NULL == currentFont->chars[high] || NULL == currentFont->chars[high][low])
 	if(maxw > 0.0)
 	{
 		while(TRUE)
 		{
 			UNICODE_TO_INT(str, i, increment)
-			c = low | (high << 8);
-			
+			FontChar *fch = FontHash_get(hash, c);
+			if(NULL == fch)
+			{
+				i += increment;
+				continue;
+			}
 			switch(c)
 			{
 				case ' ':
 						last_space = i;
 						lastw = w;
 						spaces++;
-						w += currentFont->chars[0][' ']->w;
+						w += spacew;
 						break;
 				case '\t':
 						w += spacew * 8;
@@ -196,12 +208,7 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 						end = TRUE;
 						break;
 				default:
-						if(CHAR_NOT_EXISTS)
-						{
-							i += increment;
-							continue;
-						}
-						w += currentFont->chars[high][low]->w;
+						w += fch->w;
 			}
 			if(w > maxw || '\n' == c || TRUE == end)
 			{
@@ -233,9 +240,10 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 					while(buf < last_space)
 					{
 						UNICODE_TO_INT(str,buf,incrementBuf)
-						c = low | (high << 8);
+						ch = FontHash_get(hash, c);
 						buf += incrementBuf;
-						
+						if(NULL == ch)
+							continue;
 						if('\t' == c)
 								x += spacew * 8;
 						else if(' ' == c)
@@ -247,9 +255,6 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 						}
 						else
 						{
-							if(CHAR_NOT_EXISTS)
-								continue;
-							ch = currentFont->chars[high][low];
 							DRAW_CHAR;
 						}
 					}
@@ -278,7 +283,7 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 	else
 		while(str[i]) {
 			UNICODE_TO_INT(str, i, increment)
-			c = low | (high << 8);
+			ch = FontHash_get(hash, c);
 			switch(c) {
 				case '\n':
 					x = 0;
@@ -290,9 +295,8 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
 					x += spacew * 8;
 					goto end_loop;
 			}
-			if(CHAR_NOT_EXISTS)
+			if(NULL == ch)
 				goto end_loop;
-			ch = currentFont->chars[high][low];
 			DRAW_CHAR;
 			end_loop:
 			i += increment;
@@ -312,13 +316,13 @@ void fontPrintf(Font *currentFont, const unsigned char * str, float x, float y, 
     //~ str = (char *)luaL_checkstring(L, 1);
     //~ maxw = (float)luaL_checknumber(L, 2) / currentFont->scale;
     //~ lua_newtable(L);
-    //~ 
-//~ 
+    //~
+//~
     //~ PRINT_LINES(lua_pushlstring(L, str + buf, last_space - buf);
             //~ lua_rawseti(L, -2, j++);,
             //~ lua_pushstring(L, str + buf);
             //~ lua_rawseti(L, -2, j);)
-//~ 
+//~
     //~ return 1;
 //~ }
 
@@ -356,9 +360,8 @@ void fontSetGlyph(Font *ptr, const char *line) {
 	unsigned ch = 0, ch2;
 	unsigned char char_string_buffer[9];
 	int i    = 0;
-	int high = 0;
-	int low  = 0;
-	FontChar * fch;
+	unsigned c = 0;
+	FontChar * fch = NULL;
 	if(sscanf(line, "%d %f %f %f %f %f %f %f %f", &ch, &x1, &y1, &x2, &y2, &cx1, &cy1, &w, &h) == -1)
 		return;
 	ch2 = ch;
@@ -374,38 +377,17 @@ void fontSetGlyph(Font *ptr, const char *line) {
 	UNICODE_TO_INT(char_string_buffer, 0, i)
 	else
 	{
-		myError("character %d is out of range (high: %x, low: %x)", ch2, high, low);
+		myError("character %d is out of range", ch2);
 		return;
 	}
-	if(!ptr->chars)
+	if(NULL == ptr->hash)
 	{
-		ptr->allocated = 1;
-		new(ptr->chars, FontChar**, ptr->allocated);
-		fill(ptr->chars, 0, FontChar**, ptr->allocated);
-		ptr->mem += ptr->allocated * sizeof(FontChar**);
+		ptr->hash = (void *)FontHash_new();
+		new0(fch, FontChar, 1);
+		FontHash_set((FontHash*)ptr->hash, '\0', fch);
+		fch = NULL;
 	}
-	const int incr = 1;
-	if(high + 1> ptr->allocated)
-	{
-		renew(ptr->chars, FontChar**, high + incr);
-		fill(ptr->chars + ptr->allocated, 0, FontChar**, high + incr - ptr->allocated);
-		ptr->mem += (high + incr - ptr->allocated) * sizeof(FontChar**);
-		ptr->allocated = incr + high;
-	}
-	if(!ptr->chars[high])
-	{
-		new(ptr->chars[high], FontChar*, 256);
-		fill(ptr->chars[high], 0, FontChar*, 256);
-		ptr->mem += 256 * sizeof(FontChar*);
-	}
-	if(!ptr->chars[high][low])
-	{
-		new(ptr->chars[high][low], FontChar, 1);
-		ptr->mem += sizeof(FontChar);
-	}
-	else return;
-	fch = ptr->chars[high][low];
-	
+	new0(fch, FontChar, 1);
 	x1 = x1 / (float)ptr->image->w;
 	y1 = y1 / (float)ptr->image->h;
 	cx2 = cx1 + x2;
@@ -417,38 +399,25 @@ void fontSetGlyph(Font *ptr, const char *line) {
 	memcpy(fch->v, vert, sizeof(vert));
 	memcpy(fch->t, tex, sizeof(tex));
 	fch->w = w;
-	if(' ' == low && '\0' == high)
+	FontHash_set((FontHash*)ptr->hash, c, fch);
+	if(' ' == c)
 	{
 		ptr->spacew = w;
-		if(!ptr->chars[0]['\0']) new(ptr->chars[0]['\0'], FontChar, 1);
-		if(!ptr->chars[0]['\n']) new(ptr->chars[0]['\n'], FontChar, 1);
-		if(!ptr->chars[0]['\r']) new(ptr->chars[0]['\r'], FontChar, 1);
-		if(!ptr->chars[0]['\t']) new(ptr->chars[0]['\t'], FontChar, 1);
-		ptr->mem += sizeof(FontChar) * 4;
-		ptr->chars[0]['\t']->w = w * 8;
+		fch = NULL;
+		new0(fch, FontChar, 1);
+		fch->w = w * 8;
+		FontHash_set((FontHash*)ptr->hash, '\t', fch);
 	}
 	ptr->height = h;
 }
 
 void deleteFont(Font * ptr) {
-	int i, j;
 	if(ptr)
 	{
-		if(ptr->chars)
+		if(NULL != ptr->hash)
 		{
-			for(i = 0; i < ptr->allocated; i++)
-			{
-				if(ptr->chars[i])
-				{
-					for(j = 0; j < 255; j++)
-					{
-						if(ptr->chars[i][j])
-							delete(ptr->chars[i][j]);
-					}
-					delete(ptr->chars[i]);
-				}
-			}
-			delete(ptr->chars);
+			HASH_EACH(((FontHash*)ptr->hash), delete(hashnode->value);)
+			FontHash_destroy((FontHash*)ptr->hash);
 		}
 	}
 	else
