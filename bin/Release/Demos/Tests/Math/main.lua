e77ca30616ff37094b321094c73c5b755428b6a1
Pavel Roschin <roshin@scriptumplus.ru>
2013-02-02 21:28:55 +0400

    improve vertex test

--- bin/Release/Demos/Tests/Math/main.lua
+++ bin/Release/Demos/Tests/Math/main.lua
@@ -6,11 +6,13 @@ C.printFPS = true
 local N = 256
 local img = C.generate('dummy', 1, 1)
 local start = os.clock()
-for k = 1, 300 do
+for k = 1, 100 do
 	for j = 1, N do
 		for i = 1, N do
 			img:draw(i, j, 1.5,1.5, 0.123, 0.111, 0.111)
 			img:draw(i, j, 1.5,1.5)
+			img:drawq(i, j, 1.5,1.5,1.2,1.4)
+			img:drawq(i, j, 1.5,1.5,1.2,1.4,3,3,5,2,1)
 		end
 	end
 end
