e4c8cb82b8b132174dec397965d96ddd9b51b810
Pavel Roschin <roshin@scriptumplus.ru>
2013-02-03 23:28:12 +0400

    perlin example

---
+++ bin/Release/Demos/Shaders/Perlin/main.lua
@@ -0,0 +1,39 @@
+require 'lib.cheetah'
+require 'lib.lquery.init'
+local C = cheetah
+C.init('Perlin noise', '1024x1024')
+C.printFPS = true
+local noiseSize = 32
+local noise = C.generate('noise', noiseSize, noiseSize, 'nearest')
+--~ local noisePrepare = C.newShader([[#version 120
+--~ uniform sampler2D noiseTex;
+--~ uniform vec2 texel;
+--~ varying vec2 TexCoord;
+--~ void main() {
+	--~ float n00 = perm(noise2d( x11/rep),                   pixelPosition);
+	--~ float n10 = perm(noise2d((x11 + vec2(1.0, 0.0))/rep), pixelPosition - vec2(1.0, 0.0));
+	--~ float n01 = perm(noise2d((x11 + vec2(0.0, 1.0))/rep), pixelPosition - vec2(0.0, 1.0));
+	--~ float n11 = perm(noise2d((x11 + vec2(1.0, 1.0))/rep), pixelPosition - vec2(1.0, 1.0));
+--~ }
+--~ ]])
+local fbo = C.newFramebuffer(1024,1024)
+local shader = C.newShader('perlin.glsl')
+local scale, persist, steps, contrast, brightness = 4, 0.5, 8, 1, 0
+E:new(screen):draw(function()
+	shader:bind()
+	shader:set('scale', scale, scale)
+	shader:set('persist', persist)
+	shader:set('contrast', contrast)
+	shader:set('brightness', brightness)
+	shader:set('steps', steps)
+	shader:set('texel', 1/noiseSize, 1/noiseSize)
+	shader:set('noiseSize', noiseSize, noiseSize)
+	shader:set('noiseTex', 0)
+	--~ fbo:bind()
+	noise:drawq(0, 0, 1024, 1024)
+	--~ fbo:unbind()
+	--~ shader:unbind()
+	--~ fbo:drawq(0, 0, 1024, 1024, 512, 512)
+end)
+
+C.mainLoop()
---
+++ bin/Release/Demos/Shaders/Perlin/perlin.glsl
@@ -0,0 +1,98 @@
+uniform sampler2D noiseTex;
+uniform int steps;              //4   number of iterations
+uniform vec2 scale;             //4   noise scale
+uniform float persist;          //0.5 iterations sum fading
+uniform float contrast;         //1.0 contrast
+uniform float brightness;       //0.0 brightness
+uniform vec2 texel;
+uniform vec2 noiseSize;
+varying vec2 TexCoord;
+
+/* Перлин работает на векторах, ориентированных случайным образом в пространстве.
+* У нас простой случай, 2D, поэтому просто берём единичный двумерный вектор и 
+* поворачиваем его на случайный угол. Для большей точности можно соединить две-три
+* компоненты пикселя (если брать только одну, случайных вращений будет всего 256). */
+//~ vec2 noise2d(vec2 c)
+//~ {
+	//~ vec4 tex = texture2D(noiseTex, c);
+	//~ float angle = (tex.r + tex.g * 390625e-8 /* + tex.b * 15258789e-12 */) * 8.0 * 3.1415926;
+	//~ return vec2(cos(angle), sin(angle));
+//~ }
+
+/* Вариат с выборкой просто двух значений быстрее, но мы получаем ненормированный вектор */
+vec2 noise2d(vec2 c)
+{
+	vec4 tex = texture2D(noiseTex, c);
+	return (vec2(tex.r, tex.g) - vec2(0.5)) * 4.0;
+}
+
+/* безтекстурный вариант на случай, если синус на видеокарте будет дешевле чем 4 текстурных выборки */
+//~ vec2 noise2d(vec2 c)
+//~ {
+	//~ float angle = fract(sin(dot(c, vec2(12.9898, 78.233))) * 43758.5453) * 8.0 * 3.1415926;
+	//~ return vec2(cos(angle), sin(angle));
+//~ }
+
+/* придаёт шуму гладкость */
+//~ float fade(float t) {
+	//~ return t*t*t*(t*(t*6.0-15.0)+10.0);
+//~ }
+
+/* более быстрый вариант */
+float fade(float t) {
+	return smoothstep(0., 1., t);
+}
+
+float perm(vec2 a, vec2 b)
+{
+	return dot(a, b);
+}
+
+/*
+ * 2D классический шум Перлина. Наилучшее соотношение скорость/качество.
+ */
+float noisePerlin2D(vec2 c, vec2 rep)
+{
+	/* растягиваем интервал текстурных координат от 0 до 1 до нужного нам масштаба */
+	vec2 cc = c*rep;
+	/* позиция текстеля относительно пикселя шума, который обычно намного крупнее */
+	vec2 pixelPosition = fract(cc);
+	vec2 x11 = floor(cc);
+	/* округление вектора сс даёт дискретные отсчёты шума с шагом rep */
+	const vec4 offset = vec4(0., 0., 1., 1.);
+	//~ vec4 Pi = mod(x11.xyxy + offset, rep.xyxy) * texel.xyxy;
+	/* хитрый способ добиться бесшовности: используем возможность OpenGL повторять текстуру шума */
+	vec4 Pi = (x11.xyxy + offset) / min(noiseSize.xyxy, rep.xyxy);
+	vec4 Pf = (pixelPosition.xyxy - offset);
+	
+	/* выборка четырёх значений - расстояния векторов от текущей точки */
+	float n00 = perm(noise2d(Pi.xy), Pf.xy);
+	float n10 = perm(noise2d(Pi.zy), Pf.zy);
+	float n01 = perm(noise2d(Pi.xw), Pf.xw);
+	float n11 = perm(noise2d(Pi.zw), Pf.zw);
+
+	/* Смешивание по оси Х */
+	vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade(pixelPosition.x));
+
+	/* Смешивание по оси Y */
+	float n_xy = mix(n_x.x, n_x.y, fade(pixelPosition.y));
+
+	/* Окончательное значение шума (-1, 1) */
+	return n_xy;
+}
+
+
+void main( void )
+{
+	float n = 0.0;
+	int i;
+	float mul2 = 0.5;
+	vec2 mul = scale;
+	for(i = 1; i <= steps; i++)
+	{
+		n += noisePerlin2D(TexCoord, mul) * mul2;
+		mul *= 2.0;
+		mul2 *= persist;
+	}
+	gl_FragColor = vec4((vec3(n) * contrast + 1.0 + brightness)*0.5, 1.0);
+}
