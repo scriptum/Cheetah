ec612d65c9fdd2e4dcca609bb5eadef07579de6a
Pavel Roschin <roshin@scriptumplus.ru>
2013-02-19 01:33:35 +0400

    cleanup

--- bin/Release/lib/lquery/objects.lua
+++ bin/Release/lib/lquery/objects.lua
@@ -2,26 +2,24 @@
 
 Copyright (c) 2012 Pavel Roschin (aka RPG) <rpg89@post.ru>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to 
-deal in the Software without restriction, including without limitation the 
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:  The above 
-copyright notice and this permission notice shall be included in all copies 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:  The above
+copyright notice and this permission notice shall be included in all copies
 or substantial portions of the Software.
- 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 
 ******************************************************************************]]
-
-local C = cheetah
 
 local C = cheetah
 
@@ -34,7 +32,7 @@ local image_draw_quad = function(s)
 end
 function Entity:image(image, options)
  if image and not self._image then
-		if type(image) == 'string' then 
+		if type(image) == 'string' then
 			image = C.newImage(image, options)
 		end
 		self._image = image
@@ -46,7 +44,7 @@ function Entity:image(image, options)
 end
 function Entity:imageq(image, options)
  if image and not self._image then
-		if type(image) == 'string' then 
+		if type(image) == 'string' then
 			image = C.newImage(image, options)
 		end
 		self._image = image
@@ -63,7 +61,7 @@ end
 --border-image
 function Entity:borderImage(image, top, right, bottom, left, options)
  if image and not self._image then
-		if type(image) == 'string' then 
+		if type(image) == 'string' then
 			image = C.newBorderImage(image, top, right, bottom, left, options)
 		end
 		self._image = image
