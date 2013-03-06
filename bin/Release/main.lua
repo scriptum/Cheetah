67cfb517714211cf1126a07fbdc54bb84ce072e7
Pavel Roschin <roshin@scriptumplus.ru>
2013-03-06 19:13:51 +0400

    change example title

--- bin/Release/main.lua
+++ bin/Release/main.lua
@@ -4,7 +4,13 @@ require 'lib.cheetah'
 --~ require 'lib.mixer'
 require 'lib.lquery.init'
 local C = cheetah
-C.init('Test application', '800x600 vsync')
+C.init('Welcome to Cheetah 2D Engine!', '800x600 vsync')
 
+Entity:new(screen)
+:image('Cheetahs.jpg')
+:move(-800,0)
+:delay(0.5)
+:animate({x = 0})
+:draggable()
 
 C.mainLoop()
