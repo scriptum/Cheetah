ca8681589a3745d84c3342b3c6e790e1f89bca33
Pavel Roschin <roshin@scriptumplus.ru>
2013-08-10 23:02:54 +0400

    fix distance field to new api

--- bin/Release/Demos/Text/InertialScrollText/main.lua
+++ bin/Release/Demos/Text/InertialScrollText/main.lua
@@ -7,6 +7,7 @@ C.printFPS = true
 C.autoScale(false)
 --load font
 local f = C.newFont('MonotypeCorsiva.fnt', true)
+f.distanceField = true
 -- local f = C.newFont('LiberationSerif.fnt', true)
 -- local f = C.newFont('DejaVuSans.fnt', true)
 
@@ -18,7 +19,7 @@ local str = C.getFile('bigtext.txt')
 local textarea = E:new(screen)
 :set({
 	fontScale = 0.5,  --50% font scale (21/2=10pt)
-	gamma     = 0.5,  --gamma for Distance Field
+	gamma     = 0.48,  --gamma for Distance Field
 	sharpness = 0.18, --sharpness of Distance Field anti-aliasing
 	_offset   = 0,    --others are internal
 	_velocity = 0,
@@ -35,12 +36,14 @@ local textarea = E:new(screen)
 		s._velocity = s._velocity * 0.97
 		s._lasttime = time
 	end
-	dfshader:bind()
-	dfshader:set('gamma', s.gamma)
-	dfshader:set('sharpness', s.sharpness / s.fontScale)
+	-- dfshader:bind()
+	-- dfshader:set('gamma', s.gamma)
+	-- dfshader:set('sharpness', s.sharpness / s.fontScale)
+	-- dfshader:set('size', f.image.w, f.image.h)
+	-- C.enableAlphaTest()
 	f:setScale(s.fontScale)
-	f:print(str, s.x, s.y - s._offset / s.fontScale, s.w, C.alignCenter)
-	dfshader:unbind()
+	f:print(str, s.x, s.y - s._offset, s.w, C.alignCenter)
+	-- dfshader:unbind()
 end):size(C.getWindowWidth(),C.getWindowHeight())
 :mousepressed(function(s, x, y, b)
 	s.dragY = lQuery.mY + s._offset
