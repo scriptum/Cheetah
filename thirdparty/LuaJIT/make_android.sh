f24a4dbfe3f1a07b87c45891f32878f8fcb1abdf
Pavel Roschin <roshin@scriptumplus.ru>
2013-08-25 18:17:02 +0400

    Update luaJIT

--- thirdparty/LuaJIT/make_android.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-NDK=/home/rpg/android-ndk-r8b
-NDKABI=8
-NDKVER=$NDK/toolchains/arm-linux-androideabi-4.4.3
-NDKP=$NDKVER/prebuilt/linux-x86/bin/arm-linux-androideabi-
-NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux TARGET=arm
