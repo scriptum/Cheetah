fa647b0991e6ee0c0e0ff0577385b107ede34894
Pavel Roschin <roshin@scriptumplus.ru>
2014-04-30 20:18:26 +0400

    copyright

--- src/chashtable.h
+++ src/chashtable.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
-Copyright (c) 2013 Pavel Roschin (aka RPG) <rpg89@post.ru>
+Copyright (c) 2014 Pavel Roschin (aka RPG) <rpg89@post.ru>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
